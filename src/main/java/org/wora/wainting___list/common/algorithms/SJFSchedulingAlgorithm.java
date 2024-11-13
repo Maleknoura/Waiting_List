ba91ee2d1bf15d906e76a0f8ac8f095d0458af39
Maleknoura <maleknoura098@gmail.com>
2024-11-13 11:41:12 +0100

    feat: add SJFS scheduling algorithm

---
+++ src/main/java/org/wora/wainting___list/common/algorithms/SJFSchedulingAlgorithm.java
@@ -0,0 +1,19 @@
+package org.wora.wainting___list.common.algorithms;
+
+import org.wora.wainting___list.common.SchedulingAlgorithm;
+import org.wora.wainting___list.waitingList.domain.entity.Visit;
+import org.wora.wainting___list.waitingList.domain.entity.WaintingList;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class SJFSchedulingAlgorithm implements SchedulingAlgorithm {
+
+    @Override
+    public List<Visit> schedule(WaintingList waitingList) {
+        return waitingList.getVisits().stream()
+                .sorted(Comparator.comparingLong(visit -> visit.getEstimatedProcessingTime().toMinutes()))
+                .collect(Collectors.toList());
+    }
+}
