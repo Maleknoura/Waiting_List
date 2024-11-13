df2304a08abfaf9e41745cdc7e1e1e1a2329fa67
Maleknoura <maleknoura098@gmail.com>
2024-11-13 11:34:16 +0100

    feat: add FIFO scheduling algorithm

---
+++ src/main/java/org/wora/wainting___list/common/algorithms/FIFOSchedulingAlgorithm.java
@@ -0,0 +1,19 @@
+package org.wora.wainting___list.common.algorithms;
+
+import org.wora.wainting___list.common.SchedulingAlgorithm;
+import org.wora.wainting___list.waitingList.domain.entity.Visit;
+import org.wora.wainting___list.waitingList.domain.entity.WaintingList;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FIFOSchedulingAlgorithm implements SchedulingAlgorithm {
+
+    @Override
+    public List<Visit> schedule(WaintingList waitingList) {
+        return waitingList.getVisits().stream()
+                .sorted(Comparator.comparing(Visit::getArrivalTime))
+                .collect(Collectors.toList());
+    }
+}
